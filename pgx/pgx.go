abd784294f39fd6ab1ab15435d8d1ddbaf6146c6
centny <centny@gmail.com>
2022-07-30 18:25:24 +0800

    add mocker.

--- pgx/pgx.go
+++ pgx/pgx.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/codingeasygo/crud"
+	"github.com/jackc/pgconn"
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 )
@@ -22,25 +23,139 @@ func Bootstrap(connString string) (pool *pgxpool.Pool, err error) {
 	return
 }
 
-type PgRows struct {
+var ErrNoRows = pgx.ErrNoRows
+var ErrTxClosed = pgx.ErrTxClosed
+var ErrTxCommitRollback = pgx.ErrTxCommitRollback
+
+type Row struct {
+	SQL string
+	pgx.Row
+}
+
+func (r Row) Scan(dest ...interface{}) (err error) {
+	defer func() {
+		xerr := r.Row.Scan(dest...)
+		if err == nil {
+			err = xerr
+		}
+	}()
+	err = mockerCheck("Row.Scan", r.SQL)
+	return
+}
+
+type Rows struct {
+	SQL string
 	pgx.Rows
 }
 
-func (p *PgRows) Close() (err error) {
-	p.Rows.Close()
+func (r *Rows) Scan(dest ...interface{}) error {
+	if err := mockerCheck("Rows.Scan", r.SQL); err != nil {
+		return err
+	}
+	return r.Rows.Scan(dest...)
+}
+
+func (r *Rows) Values() ([]interface{}, error) {
+	if err := mockerCheck("Rows.Values", r.SQL); err != nil {
+		return nil, err
+	}
+	return r.Rows.Values()
+}
+
+func (r *Rows) Close() (err error) {
+	r.Rows.Close()
 	return
 }
 
-type TxQueryer struct {
+type BatchResults struct {
+	pgx.BatchResults
+}
+
+func (b *BatchResults) Exec() (pgconn.CommandTag, error) {
+	if err := mockerCheck("BatchResult.Exec", ""); err != nil {
+		return nil, err
+	}
+	return b.BatchResults.Exec()
+}
+
+func (b *BatchResults) Query() (rows *Rows, err error) {
+	if err := mockerCheck("BatchResult.Query", ""); err != nil {
+		return nil, err
+	}
+	raw, err := b.BatchResults.Query()
+	if err == nil {
+		rows = &Rows{Rows: raw}
+	}
+	return
+}
+
+func (b *BatchResults) QueryRow() *Row {
+	return &Row{Row: b.BatchResults.QueryRow()}
+}
+
+func (b *BatchResults) Close() error {
+	if err := mockerCheck("BatchResult.Close", ""); err != nil {
+		return err
+	}
+	return b.BatchResults.Close()
+}
+
+type Tx struct {
 	pgx.Tx
 }
 
-func NewTxQueryer(tx pgx.Tx) (queryer *TxQueryer) {
-	queryer = &TxQueryer{Tx: tx}
+// Begin starts a pseudo nested transaction.
+func (t *Tx) Begin(ctx context.Context) (tx *Tx, err error) {
+	if err := mockerCheck("Tx.Begin", ""); err != nil {
+		return nil, err
+	}
+	raw, err := t.Tx.Begin(ctx)
+	if err == nil {
+		tx = &Tx{Tx: raw}
+	}
 	return
 }
 
-func (t *TxQueryer) Exec(ctx context.Context, sql string, args ...interface{}) (insertId, affected int64, err error) {
+func (t *Tx) Commit(ctx context.Context) error {
+	if err := mockerCheck("Tx.Commit", ""); err != nil {
+		t.Tx.Rollback(ctx)
+		return err
+	}
+	return t.Tx.Commit(ctx)
+}
+
+func (t *Tx) Rollback(ctx context.Context) error {
+	if err := mockerCheck("Tx.Rollback", ""); err != nil {
+		t.Tx.Rollback(ctx)
+		return err
+	}
+	return t.Tx.Rollback(ctx)
+}
+
+func (t *Tx) CopyFrom(ctx context.Context, tableName pgx.Identifier, columnNames []string, rowSrc pgx.CopyFromSource) (int64, error) {
+	if err := mockerCheck("Tx.CopyFrom", ""); err != nil {
+		return 0, err
+	}
+	return t.Tx.CopyFrom(ctx, tableName, columnNames, rowSrc)
+}
+
+func (t *Tx) SendBatch(ctx context.Context, b *pgx.Batch) *BatchResults {
+	return &BatchResults{
+		BatchResults: t.Tx.SendBatch(ctx, b),
+	}
+}
+
+func (t *Tx) Prepare(ctx context.Context, name, sql string) (*pgconn.StatementDescription, error) {
+	if err := mockerCheck("Tx.Prepare", sql); err != nil {
+		return nil, err
+	}
+	return t.Tx.Prepare(ctx, name, sql)
+}
+
+func (t *Tx) Exec(ctx context.Context, sql string, args ...interface{}) (insertId, affected int64, err error) {
+	if err := mockerCheck("Tx.Exec", sql); err != nil {
+		return 0, 0, err
+	}
 	res, err := t.Tx.Exec(ctx, sql, args...)
 	if err == nil {
 		affected = res.RowsAffected()
@@ -48,7 +163,10 @@ func (t *TxQueryer) Exec(ctx context.Context, sql string, args ...interface{}) (
 	return
 }
 
-func (t *TxQueryer) ExecRow(ctx context.Context, sql string, args ...interface{}) (insertId int64, err error) {
+func (t *Tx) ExecRow(ctx context.Context, sql string, args ...interface{}) (insertId int64, err error) {
+	if err := mockerCheck("Tx.ExecRow", sql); err != nil {
+		return 0, err
+	}
 	insertId, affected, err := t.Exec(ctx, sql, args...)
 	if err == nil && affected < 1 {
 		err = pgx.ErrNoRows
@@ -56,16 +174,50 @@ func (t *TxQueryer) ExecRow(ctx context.Context, sql string, args ...interface{}
 	return
 }
 
-func (t *TxQueryer) Query(ctx context.Context, sql string, args ...interface{}) (rows crud.Rows, err error) {
+func (t *Tx) Query(ctx context.Context, sql string, args ...interface{}) (rows crud.Rows, err error) {
+	if err := mockerCheck("Tx.Query", sql); err != nil {
+		return nil, err
+	}
 	raw, err := t.Tx.Query(ctx, sql, args...)
 	if err == nil {
-		rows = &PgRows{Rows: raw}
+		rows = &Rows{SQL: sql, Rows: raw}
 	}
 	return
 }
 
-func (t *TxQueryer) QueryRow(ctx context.Context, sql string, args ...interface{}) (row crud.Row) {
-	row = t.Tx.QueryRow(ctx, sql, args...)
+func (t *Tx) QueryRow(ctx context.Context, sql string, args ...interface{}) crud.Row {
+	return &Row{
+		SQL: sql,
+		Row: t.Tx.QueryRow(ctx, sql, args...),
+	}
+}
+
+func (t *Tx) CrudExec(ctx context.Context, sql string, args ...interface{}) (insertId, affected int64, err error) {
+	if err := mockerCheck("Tx.Exec", sql); err != nil {
+		return 0, 0, err
+	}
+	insertId, affected, err = t.Exec(ctx, sql, args...)
+	return
+}
+
+func (t *Tx) CrudExecRow(ctx context.Context, sql string, args ...interface{}) (insertId int64, err error) {
+	if err := mockerCheck("Tx.ExecRow", sql); err != nil {
+		return 0, err
+	}
+	insertId, err = t.ExecRow(ctx, sql, args...)
+	return
+}
+
+func (t *Tx) CrudQuery(ctx context.Context, sql string, args ...interface{}) (rows crud.Rows, err error) {
+	if err := mockerCheck("Tx.Query", sql); err != nil {
+		return nil, err
+	}
+	rows, err = t.Query(ctx, sql, args...)
+	return
+}
+
+func (t *Tx) CrudQueryRow(ctx context.Context, sql string, args ...interface{}) (row crud.Row) {
+	row = t.QueryRow(ctx, sql, args...)
 	return
 }
 
@@ -78,39 +230,115 @@ func NewPgQueryer(pool *pgxpool.Pool) (queryer *PgQueryer) {
 	return
 }
 
-func (d *PgQueryer) Begin(ctx context.Context) (tx *TxQueryer, err error) {
-	raw, err := d.Pool.Begin(ctx)
-	if err == nil {
-		tx = NewTxQueryer(raw)
+func (p *PgQueryer) Exec(ctx context.Context, sql string, args ...interface{}) (insertId, affected int64, err error) {
+	if err := mockerCheck("Pool.Exec", sql); err != nil {
+		return 0, 0, err
 	}
-	return
-}
-
-func (d *PgQueryer) Exec(ctx context.Context, sql string, args ...interface{}) (insertId, affected int64, err error) {
-	res, err := d.Pool.Exec(ctx, sql, args...)
+	res, err := p.Pool.Exec(ctx, sql, args...)
 	if err == nil {
 		affected = res.RowsAffected()
 	}
 	return
 }
 
-func (d *PgQueryer) ExecRow(ctx context.Context, sql string, args ...interface{}) (insertId int64, err error) {
-	insertId, affected, err := d.Exec(ctx, sql, args...)
+func (p *PgQueryer) ExecRow(ctx context.Context, sql string, args ...interface{}) (insertId int64, err error) {
+	if err := mockerCheck("Pool.ExecRow", sql); err != nil {
+		return 0, err
+	}
+	insertId, affected, err := p.Exec(ctx, sql, args...)
 	if err == nil && affected < 1 {
 		err = pgx.ErrNoRows
 	}
 	return
 }
 
-func (d *PgQueryer) Query(ctx context.Context, sql string, args ...interface{}) (rows crud.Rows, err error) {
-	raw, err := d.Pool.Query(ctx, sql, args...)
+func (p *PgQueryer) Query(ctx context.Context, sql string, args ...interface{}) (rows crud.Rows, err error) {
+	if err := mockerCheck("Pool.Query", sql); err != nil {
+		return nil, err
+	}
+	raw, err := p.Pool.Query(ctx, sql, args...)
 	if err == nil {
-		rows = &PgRows{Rows: raw}
+		rows = &Rows{SQL: sql, Rows: raw}
 	}
 	return
 }
 
-func (d *PgQueryer) QueryRow(ctx context.Context, sql string, args ...interface{}) (row crud.Row) {
-	row = d.Pool.QueryRow(ctx, sql, args...)
+func (p *PgQueryer) QueryRow(ctx context.Context, sql string, args ...interface{}) crud.Row {
+	return &Row{
+		SQL: sql,
+		Row: p.Pool.QueryRow(ctx, sql, args...),
+	}
+}
+
+func (p *PgQueryer) CrudExec(ctx context.Context, sql string, args ...interface{}) (insertId, affected int64, err error) {
+	if err := mockerCheck("Pool.Exec", sql); err != nil {
+		return 0, 0, err
+	}
+	insertId, affected, err = p.Exec(ctx, sql, args...)
 	return
+}
+
+func (p *PgQueryer) CrudExecRow(ctx context.Context, sql string, args ...interface{}) (insertId int64, err error) {
+	if err := mockerCheck("Pool.ExecRow", sql); err != nil {
+		return 0, err
+	}
+	insertId, err = p.ExecRow(ctx, sql, args...)
+	return
+}
+
+func (p *PgQueryer) CrudQuery(ctx context.Context, sql string, args ...interface{}) (rows crud.Rows, err error) {
+	if err := mockerCheck("Pool.Query", sql); err != nil {
+		return nil, err
+	}
+	rows, err = p.Query(ctx, sql, args...)
+	return
+}
+
+func (p *PgQueryer) CrudQueryRow(ctx context.Context, sql string, args ...interface{}) (row crud.Row) {
+	row = p.QueryRow(ctx, sql, args...)
+	return
+}
+
+func (p *PgQueryer) CopyFrom(ctx context.Context, tableName pgx.Identifier, columnNames []string, rowSrc pgx.CopyFromSource) (int64, error) {
+	if err := mockerCheck("Pool.CopyFrom", ""); err != nil {
+		return 0, err
+	}
+	return p.Pool.CopyFrom(ctx, tableName, columnNames, rowSrc)
+}
+
+func (p *PgQueryer) SendBatch(ctx context.Context, b *pgx.Batch) *BatchResults {
+	return &BatchResults{
+		BatchResults: p.Pool.SendBatch(ctx, b),
+	}
+}
+
+func (p *PgQueryer) Begin(ctx context.Context) (tx *Tx, err error) {
+	if err := mockerCheck("Pool.Begin", ""); err != nil {
+		return nil, err
+	}
+	raw, err := p.Pool.Begin(ctx)
+	if err == nil {
+		tx = &Tx{Tx: raw}
+	}
+	return
+}
+
+func Exec(ctx context.Context, sql string, args ...interface{}) (insertId, affected int64, err error) {
+	return Shared.Exec(ctx, sql, args...)
+}
+
+func ExecRow(ctx context.Context, sql string, args ...interface{}) (insertId int64, err error) {
+	return Shared.ExecRow(ctx, sql, args...)
+}
+
+func QueryRow(ctx context.Context, sql string, args ...interface{}) crud.Row {
+	return Shared.QueryRow(ctx, sql, args...)
+}
+
+func Query(ctx context.Context, sql string, args ...interface{}) (rows crud.Rows, err error) {
+	return Shared.Query(ctx, sql, args...)
+}
+
+func Begin(ctx context.Context) (tx *Tx, err error) {
+	return Shared.Begin(ctx)
 }
